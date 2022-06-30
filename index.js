444e2f005598d162eaf90ed0492184b0a58cd49c
Mazharul islam <mazharulislam.dev@gmail.com>
2022-06-30 13:12:08 +0600

    crate get api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nagglkh.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -19,13 +20,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const billCollection = client.db('billAll').collection('bill')
-
+        const infoCollection = client.db('billInfo').collection('info')
+        // post info
+        app.post('/info', async (req, res) => {
+            const newInfo = req.body;
+            const result = await infoCollection.insertOne(newInfo);
+            res.send(result)
+        })
+        //get info
+        app.get('/info', async (req, res) => {
+            const info = await infoCollection.find().toArray();
+            res.send(info);
+        })
     }
     finally {
 
     }
 }
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('running test')
